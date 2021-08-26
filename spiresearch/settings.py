7ee923d4bdffb71b197a7de08952e2ba26b2a5c5
anujmittal94 <anujanilmittal@gmail.com>
2021-08-26 15:48:43 +0400

    Env changes

--- spiresearch/settings.py
+++ spiresearch/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 # SECRET_KEY = 'django-insecure-k6l=mv8adjmt1wyt+uyqgr4d=-%==n7*g5hvtdi-8w%w-p$8s^'
-SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-k6l=mv8adjmt1wyt+uyqgr4d=-%==n7*g5hvtdi-8w%w-p$8s^')
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = True
